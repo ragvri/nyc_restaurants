0f9c57e2b3450aba8b124a6957f799b35f103fd9
Po-Yu Huang <poyu0987@gmail.com>
2020-04-09 14:44:59 -0400

    yelp scrapper, by phone number

---
+++ yelp_scrapper.py
@@ -0,0 +1,22 @@
+from credential import Yelp
+import json, requests
+
+def get_info(phone, locale='en_US'):
+    if '+1' != phone[0:2]:
+        phone = '+1' + phone
+
+    headers = {'Authorization': 'Bearer %s' % Yelp.KEY.value}
+    url='https://api.yelp.com/v3/businesses/search/phone'
+    params = {'phone': phone, 'locale': locale}
+    req = requests.get(url, params=params, headers=headers)
+
+    # proceed only if the status code is 200
+    if req.status_code != 200:
+        print('The status code is {}, please try again.'.format(req.status_code))
+        return []
+    # printing the text from the response
+    result = json.loads(req.text)
+    return (result['businesses'])
+
+if __name__ == '__main__':
+    print(get_info('2129577500'))
