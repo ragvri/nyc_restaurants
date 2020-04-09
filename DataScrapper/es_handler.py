6510c2327e21b39b0111578ae95b86146fa4ac71
Po-Yu Huang <poyu0987@gmail.com>
2020-04-09 17:42:37 -0400

    add ElasticSearch handler

---
+++ DataScrapper/es_handler.py
@@ -0,0 +1,23 @@
+import json
+
+def update_restaurants(restaurants, new_rest):
+    requried_col = set(['boro', 'zipcode', 'inspection_date', 'score', 'grade',
+                        'latitude', 'longitude', 'rating', 'price', 'categories',
+                        'phone', 'name'])
+    for key in list(new_rest.keys()):
+        if key not in requried_col:
+            new_rest.pop(key)
+        if key == 'rating':
+            new_rest[key] = float(new_rest[key])
+        if key in ('latitude', 'longitude'):
+            # json can't handle Decimal
+            new_rest[key] = str(new_rest[key])
+    restaurants.append({"index": {"_index": 'restaurants', '_id': len(restaurants),
+                                  '_type': 'Restaurant'}})
+    restaurants.append(new_rest)
+    return restaurants
+
+def info2json(restaurants):
+    with open('ESinfo.json', 'w') as f:
+        for row in restaurants:
+            json.dump(row, f)
