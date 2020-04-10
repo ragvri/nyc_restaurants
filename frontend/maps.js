cec1574ce47b95b41d020ed2c5787df9cbac522a
ragvri <raghavjindal2003@gmail.com>
2020-04-10 14:02:00 -0400

    added new code

--- frontend/maps.js
+++ frontend/maps.js
@@ -147,3 +147,23 @@ function initMap() {
         }
     });
 }
+
+var checkList = document.getElementById('search_price');
+var items = document.getElementById('items');
+checkList.getElementsByClassName('anchor')[0].onclick = function (evt) {
+    if (items.classList.contains('visible')) {
+        items.classList.remove('visible');
+        items.style.display = "none";
+    }
+
+    else {
+        items.classList.add('visible');
+        items.style.display = "block";
+    }
+
+
+}
+
+items.onblur = function (evt) {
+    items.classList.remove('visible');
+}
