c0b185bfda169273cb8f57596bd15f9453ea7f00
ragvri <raghavjindal2003@gmail.com>
2020-04-11 15:09:06 -0400

    working frontend for search

--- frontend/js/sidebar.js
+++ frontend/js/sidebar.js
@@ -1,11 +1,9 @@
 function openNav() {
     document.getElementById("searchSideBar").style.width = "170px";
-    document.getElementById("main").style.marginLeft = "250px";
-    document.getElementById("searchSideBar").style.left = "10px"
+    document.getElementById("searchSideBar").style.boxShadow = "7px 7px"
 }
 
 /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
 function closeNav() {
     document.getElementById("searchSideBar").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
 }
